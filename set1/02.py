37de5940d7ee55f4454cc6ea385b177f266f66e1
Zach Taylor <zbtaylor1@gmail.com>
2018-05-28 18:21:29 -0400

    removed unnecessary functions in challenge 1, tidied up challenge 2

--- set1/02.py
+++ set1/02.py
@@ -9,6 +9,5 @@ def fixed_xor(string, mask):
 
 example_string = '1c0111001f010100061a024b53535009181c'
 example_mask = '686974207468652062756c6c277320657965'
-
 result = fixed_xor(example_string, example_mask)
 print(result) # expected: 746865206b696420646f6e277420706c6179
