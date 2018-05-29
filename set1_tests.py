50ad05559fc7e0bfd3f5b392fa7e0f727c5e45f3
Zach Taylor <zbtaylor1@gmail.com>
2018-05-29 11:59:59 -0400

    made actual unit tests

--- set1_tests.py
+++ set1_tests.py
@@ -1,20 +1,28 @@
+import unittest
 import set1.module as s1
 
-# challenge 1
-c1_hex = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-c1_result = s1.hex_to_base64(c1_hex)
-print('Challenge 1:')
-print(c1_result) # expected: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+class TestSet1(unittest.TestCase):
 
-# challenge 2
-c2_hex = '1c0111001f010100061a024b53535009181c'
-c2_mask = '686974207468652062756c6c277320657965'
-c2_result = s1.fixed_xor(c2_hex, c2_mask)
-print('Challenge 2:')
-print(c2_result) # expected: 746865206b696420646f6e277420706c6179
+	def test_one(self):
+		hex_s = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+		guess = s1.hex_to_base64(hex_s)
+		answer = b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+		self.assertEqual(guess, answer)
 
-# challenge 3
-c3_hex = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
-c3_result = s1.decrypt(c3_hex)
-print('Challenge 3:')
-print(c3_result)
+
+	def test_two(self):
+		hex_s = '1c0111001f010100061a024b53535009181c'
+		mask = '686974207468652062756c6c277320657965'
+		guess = s1.fixed_xor(hex_s, mask)
+		answer = '746865206b696420646f6e277420706c6179'
+		self.assertEqual(guess, answer)
+
+
+	def test_three(self):
+		hex_s = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736' 
+		guess = s1.decrypt(hex_s)
+		answer = b"Cooking MC's like a pound of bacon"
+		self.assertEqual(guess, answer)
+
+if __name__ == '__main__':
+    unittest.main()
