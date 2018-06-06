b58bda5b6b5c2c608b513479a0be773dfb5046f4
Zach Taylor <zbtaylor1@gmail.com>
2018-06-06 15:14:59 -0400

    added docstrings and more cleanup

--- set1/module.py
+++ set1/module.py
@@ -54,19 +54,61 @@ BIGRAM_FREQS = {
 }
 
 
+###############################
+# ALWAYS OPERATE ON RAW BYTES #
+###############################
+
+
 def hex_to_base64(hexstr):
-	return base64.b64encode(bytes.fromhex(hexstr))
+	'''Converts hexadecimal to base64.
+	
+	Args:
+	hexstr (str): String of hex digits to be converted.
+
+	Returns:
+	bytes: Base64 encoded bytes literal.
+	'''
+	raw_bytes = bytes.fromhex(hexstr)
+	return base64.b64encode(raw_bytes)
 
 
 def fixed_length_xor(hexstr, hexmask):
-	return bytes([a ^ b for a, b in zip(bytes.fromhex(hexstr), bytes.fromhex(hexmask))])
+	'''XOR two hexadecimal strings of matching length.
+
+	Args:
+	hexstr (str): String of hex digits to be encrypted.
+	hexmask (str): String of hex digits to XOR against.
+
+	Returns:
+	bytes: Hexadecimal encoded bytes literal.
+	'''
+	bytes1, bytes2 = bytes.fromhex(hexstr), bytes.fromhex(hexmask)
+	return bytes([a ^ b for a, b in zip(bytes1, bytes2)])
 
 
 def single_char_xor(hexstr, key):
+	'''XOR a hexadecimal string against a single key.
+
+	Args:
+	hexstr (str): String of hex digits to be encrypted.
+	key (str): The single character to XOR against.
+
+	Returns:
+	bytes: Hexadecimal encoded bytes literal.
+	'''
 	return bytes([a ^ ord(key) for a in bytes.fromhex(hexstr)])
 
 
 def repeating_key_xor(string, key):
+	'''XOR a string against a repeating key.
+
+	Args:
+	string (str): The string to be encrypted.
+	key (str): The key to be repeated and XOR'ed against.
+
+	Returns:
+	bytearray: A bytearray containing the encrypted bytes.
+	'''
 	result = bytearray(b'')
 	i = 0
 	for c in string:
@@ -75,10 +117,23 @@ def repeating_key_xor(string, key):
 			i += 1
 		else:
 			i = 0
-	return result.hex()
+	return result
 
 
 def score_string(bstring):
+	'''Score the likelihood that a string contains English text.
+	
+	Decodes the bytes literal and then runs through it looking for common English
+	bigrams (sequence of two adjacent letters). Add the frequency of a bigram to
+	the score every time one is found. Strings with higher scores are more likely
+	to be English.
+
+	Args:
+	bstring (bytes): Bytes literal to be decoded and scored.
+
+	Returns:
+	int: The sum of frequency values for all bigrams found.
+	'''
 	score = 0
 	bigrams = []
 	string = ''
@@ -95,6 +150,18 @@ def score_string(bstring):
 
 
 def find_single_key(hexstr):
+	'''Finds the best single character XOR key to decrypt a string.
+
+	XORs the given string against every printable character, scores each result,
+	and keeps track of the best score. Returns the best result based on simple
+	bigram frequency analysis.
+
+	Args:
+	hexstr (str): The hex string to XOR.
+
+	Returns:
+	(int, bytes): Returns the best score and accompanying result as a tuple.
+	'''
 	result = b''
 	best_score = 0
 	for c in string.printable:
@@ -107,6 +174,14 @@ def find_single_key(hexstr):
 
 
 def find_in_list(hexlist):
+	'''Finds the line in a list that has been single character XOR'ed.
+
+	Args:
+	hexlist (list): List of hex strings to sift through.
+
+	Return:
+	bytes: Bytes literal of the decrypted hex with the highest score.
+	'''
 	result = b''
 	best_score = 0
 	for l in hexlist:
@@ -117,23 +192,48 @@ def find_in_list(hexlist):
 	return result
 
 
-def build_corpus_from_file(file):
-	file = open(file)
+def build_corpus_from_file(filepath):
+	'''Takes a text file and appends each line to a list
+
+	Args:
+	file (str): Path to text file.
+
+	Returns:
+	list: A list containing each line of text as a string.
+	'''
+	file = open(filepath)
 	result = [line.rstrip('\n') for line in file]
 	file.close()
 	return result
 
 
 def string_to_bits(string):
+	'''Converts a string to it's binary representation.
+
+	Args:
+	string (str): The string to convert.
+
+	Returns:
+	string: A string containing only ones and zeroes.
+	'''
 	result = ''
 	for c in string:
 		bits = bin(ord(c))[2:]
-		bits = '00000000'[len(bits):] + bits # makes sure each c is turned in to 8 full bits
+		bits = '00000000'[len(bits):] + bits # makes sure each is a 8 full bits
 		result += bits
 	return result
 
 
 def hamming_distance(str1, str2):
+	'''Computes the hamming distance between two same-length strings.
+
+	Args:
+	str1 (str): First string to compare.
+	str2 (str): Second string to compare.
+
+	Returns:
+	int: Sum of bits that differ between the two strings.
+	'''
 	bits1 = string_to_bits(str1)
 	bits2 = string_to_bits(str2)
 	return sum([ord(a) ^ ord(b) for a, b in zip(bits1, bits2)])
--- set1_tests.py
+++ set1_tests.py
@@ -4,38 +4,38 @@ import set1.module as s1
 class TestSet1(unittest.TestCase):
 
 	def test_one(self):
-		hex_s = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-		guess = s1.hex_to_base64(hex_s)
-		answer = b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+		hexstr = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+		guess = s1.hex_to_base64(hexstr).decode('ascii')
+		answer = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 		self.assertEqual(guess, answer)
 
 
 	def test_two(self):
-		hex_s = '1c0111001f010100061a024b53535009181c'
-		mask = '686974207468652062756c6c277320657965'
-		guess = s1.fixed_length_xor(hex_s, mask)
-		answer = bytes.fromhex('746865206b696420646f6e277420706c6179')
+		hexstr = '1c0111001f010100061a024b53535009181c'
+		hexmask = '686974207468652062756c6c277320657965'
+		guess = s1.fixed_length_xor(hexstr, hexmask).hex()
+		answer = '746865206b696420646f6e277420706c6179'
 		self.assertEqual(guess, answer)
 
 
 	def test_three(self):
-		hex_s = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736' 
-		guess = s1.find_single_key(hex_s)
-		answer = b'Cooking MC\'s like a pound of bacon'
-		self.assertEqual(guess[1], answer)
+		hexstr = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736' 
+		guess = s1.find_single_key(hexstr)[1].decode('ascii')
+		answer = "Cooking MC's like a pound of bacon"
+		self.assertEqual(guess, answer)
 
 
 	def test_four(self):
 		hex_lines = s1.build_corpus_from_file('./data/set1challenge4.txt')
-		guess = s1.find_in_list(hex_lines)
-		answer = b'Now that the party is jumping\n'
+		guess = s1.find_in_list(hex_lines).decode('ascii')
+		answer = 'Now that the party is jumping\n'
 		self.assertEqual(guess, answer)
 
 
 	def test_five(self):
 		to_encode = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
 		key = 'ICE'
-		guess = s1.repeating_key_xor(to_encode, key)
+		guess = s1.repeating_key_xor(to_encode, key).hex()
 		answer = '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
 		self.assertEqual(guess, answer)
 
@@ -43,8 +43,6 @@ class TestSet1(unittest.TestCase):
 	def test_six(self):
 		str1 = 'this is a test'
 		str2 = 'wokka wokka!!!'
-		# str1 = '111110'
-		# str2 = '000001'
 		guess = s1.hamming_distance(str1, str2)
 		answer = 37
 		self.assertEqual(guess, answer)
